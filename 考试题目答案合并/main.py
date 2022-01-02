1791f5d01ff9a3387d66c4840abc5d0cba7dcca1
haoxuan <283731869@qq.com>
2022-01-02 14:59:46 +0800

    匹配html

--- 考试题目答案合并/main.py
+++ 考试题目答案合并/main.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from lxml import etree
+
+# 读取文章题目，取出名称和数据
+def readTopic():
+    try:
+        openFile = open("./网页设计与制作.html", "r")
+        data = openFile.read()
+        html = etree.HTML(data)
+        for element in html.xpath('//div[@class="st_xx"]'):
+            print(element)
+        print(len(html.xpath('//div[@class="st_xx"]')))
+        print(len(html.xpath('//div[@class="st_tm"]')))
+    finally:
+        openFile.close()
+
 if __name__ == "__main__":
-    print("233")
-    print(4 * 23)
+    readTopic()
